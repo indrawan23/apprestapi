37d946c5fa4f94de0df3fc3f206f8fc7a5be9728
Indrawan Saputra <indrahtcblog@gmail.com>
2022-11-07 11:13:25 +0700

    berhasil menampilkan semua data agenda

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ let response = require("./res");
 let connection = require("./koneksi");
 
 exports.index = function (req, res) {
-  response.ok("Aplikasi REST API berjalan!");
+  response.ok("Aplikasi REST API berjalan!", res);
+};
+
+// menampilkan semua data agenda
+exports.tampilagenda = function (req, res) {
+  connection.query("SELECT * FROM agenda", function (error, rows, fileds) {
+    if (error) {
+      connection.log(error);
+    } else {
+      response.ok(rows, res);
+    }
+  });
 };
