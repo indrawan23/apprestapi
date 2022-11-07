934fce9ac835f55f2b2858a86e86bba006512aca
Indrawan Saputra <indrahtcblog@gmail.com>
2022-11-07 15:47:31 +0700

    berhasil menampilkan data by id

--- controller.js
+++ controller.js
@@ -2,18 +2,35 @@
 
 let response = require("./res");
 let connection = require("./koneksi");
+const conn = require("./koneksi");
 
 exports.index = function (req, res) {
   response.ok("Aplikasi REST API berjalan!", res);
 };
 
 // menampilkan semua data agenda
-exports.tampilagenda = function (req, res) {
-  connection.query("SELECT * FROM agenda", function (error, rows, fileds) {
+exports.tampilAgenda = function (req, res) {
+  connection.query("SELECT * FROM agenda", function (error, rows, fields) {
     if (error) {
-      connection.log(error);
+      console.log(error);
     } else {
       response.ok(rows, res);
     }
   });
+};
+
+// menampilkan data agenda by ID
+exports.tampilAgendaById = function (req, res) {
+  let id = req.params.id;
+  connection.query(
+    "SELECT * FROM agenda WHERE id_agenda =?",
+    [id],
+    function (error, rows, fields) {
+      if (error) {
+        console.log(error);
+      } else {
+        response.ok(rows, res);
+      }
+    }
+  );
 };
--- routes.js
+++ routes.js
@@ -4,5 +4,6 @@ module.exports = function (app) {
   let jsonku = require("./controller");
 
   app.route("/").get(jsonku.index);
-  app.route("/tampil").get(jsonku.tampilagenda);
+  app.route("/tampil").get(jsonku.tampilAgenda);
+  app.route("/tampil/:id").get(jsonku.tampilAgendaById);
 };
